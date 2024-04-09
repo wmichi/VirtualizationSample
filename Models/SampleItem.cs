e7845c6764140236fd8224a7550647d382a29435
wmichi <waku.michi1227@gmail.com>
2024-04-10 02:26:15 +0900

    first commit

---
+++ Models/SampleItem.cs
@@ -0,0 +1,14 @@
+﻿namespace VirtualizationSample.Models
+{
+    public class SampleItem
+    {
+        public int Id { get; set; }
+        public string Summary { get; set; }
+
+        public SampleItem(int id)
+        {
+            Id = id;
+            Summary = $"Item id: {id}";
+        }
+    }
+}
